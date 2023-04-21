a4bcb3837dee9ddf7b57f3b18f0544b39fc78bac
Hossein <hossein.saafi@gmail.com>
2023-04-20 19:20:12 -0700

    Assert + Emoji

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+      console.log(`🍏🍏Assertion Passed: ${actual} === ${expected}`);
+  } else {
+      console.log(`🍎🍎Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
