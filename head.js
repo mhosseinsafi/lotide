de0a29583402f941dc1155b3de59be1afe54f02b
Hossein <hossein.saafi@gmail.com>
2023-04-21 18:58:25 -0700

    Implement head

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🍏🍏Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍎🍎Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let head = function(arr) {
+  return arr[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
