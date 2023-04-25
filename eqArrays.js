89145989576a3ad0d906240e202d265dbea94eba
Hossein <hossein.saafi@gmail.com>
2023-04-24 19:11:12 -0700

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🍏🍏Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍎🍎Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+let eqArrays = function(arr1,arr2) {
+  let flag = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      flag = false;
+    }
+    if (arr1.length !== arr2.length) {
+      flag = false;
+    }
+  } return flag;
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+
+
