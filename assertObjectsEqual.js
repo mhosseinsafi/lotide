1ad36cbc8792386af5ad2aec6cd160202199a4e0
Hossein <hossein.saafi@gmail.com>
2023-04-30 17:32:16 -0700

    Implement assertObjectsEqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,33 @@
+const eqObjects = (obj1, obj2) => {
+  const array1 = Object.keys(obj1);
+  const array2 = Object.keys(obj2);
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (const key of array1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual,expected)) {
+    console.log(`🍏🍏Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🍎🍎Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { a: 1 , b: 2};
+
+assertObjectsEqual(obj1, obj2);
+
+
+
+  
+
+
