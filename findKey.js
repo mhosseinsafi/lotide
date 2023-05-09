ee9ff85b026925caf6518e2314e35ed229739d84
Hossein <hossein.saafi@gmail.com>
2023-05-09 09:41:18 -0700

    Implement findKey

---
+++ findKey.js
@@ -0,0 +1,32 @@
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+console.log(result); // "noma"
+
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+      console.log(`🍏🍏Assertion Passed: ${actual} === ${expected}`);
+  } else {
+      console.log(`🍎🍎Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
