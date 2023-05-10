73875c6d1b348ca266993c0c8fde132637cbbddd
Hossein <hossein.saafi@gmail.com>
2023-05-10 13:07:14 -0700

    Head

--- head.js
+++ head.js
@@ -5,10 +5,9 @@ let head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
-module.exports = assertEqual;
+
+module.exports = head;
 
 
 
