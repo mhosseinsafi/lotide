be3fd6d697b6c43f9a12671ef8a1a90769a512cb
Hossein <hossein.saafi@gmail.com>
2023-05-10 13:59:18 -0700

    Assert Arrays Equal Test

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,10 @@
+
+
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
