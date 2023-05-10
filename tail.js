ed88b06475936db129308a9604f9de1b848f5104
Hossein <hossein.saafi@gmail.com>
2023-05-10 13:10:43 -0700

    Tail

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`🍏🍏Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🍎🍎Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 let tail = function(arr) {
   let x = [];
@@ -15,9 +8,5 @@ let tail = function(arr) {
   return x;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-
+module.exports = tail;
