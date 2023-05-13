3a36cc5278326f87e530806803dfae93fc6845ac
Hossein <hossein.saafi@gmail.com>
2023-05-12 23:01:04 -0700

    Without

--- without.js
+++ without.js
@@ -1,24 +1,6 @@
-const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
-    console.log(`🍏🍏Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🍎🍎Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-const eqArrays = function(arr1,arr2) {
-  let flag = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      flag = false;
-    }
-    if (arr1.length !== arr2.length) {
-      flag = false;
-    }
-  } return flag;
-};
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
 
 const without = function(input, itemsToRemove) {
   const output = [];
@@ -31,21 +13,6 @@ const without = function(input, itemsToRemove) {
   return output;
 };
 
-const data = without([1,23,3, "hello"], ["hello"]);
-
-//should pass
-assertEqual(eqArrays(data, [1,23,3]), true);
-
-//should fail
-assertEqual(eqArrays(data, [1,23]), false);
-
-// without([1, 2, 3], [1]) // => [2, 3]
-// without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
-
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); // no need to capture return value for this test case
-// // Make sure the original array was not altered by the without function
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
+module.exports = without;
 
 
