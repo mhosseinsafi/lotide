a8e0a1fc89a28d025583eab6b58d4a4ecc43eecc
Hossein <hossein.saafi@gmail.com>
2023-05-12 23:11:56 -0700

    Find Key Test

---
+++ test/findKeyTest.js
@@ -0,0 +1,16 @@
+
+const assert = require('chai').assert;
+const findKey   = require('../findKey');
+
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+
+console.log(result); // "noma"
