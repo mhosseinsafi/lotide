8985ce419ad966527650f554da3bcda03188eea8
Hossein <hossein.saafi@gmail.com>
2023-05-12 23:06:38 -0700

    Count Letters

--- countLetters.js
+++ countLetters.js
@@ -1,29 +1,22 @@
-const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
-    console.log(`🍏🍏Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🍎🍎Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const countLetters = function (sentence) {
+const countLetters = function(sentence) {
   const results = {};
   for (const char of sentence) {
-   if(char !== " ") {
-    if (!results[char]) {
-      results[char] = 1;
+    if (char !== " ") {
+      if (!results[char]) {
+        results[char] = 1;
+      } else {
+        results[char]++;
+      }
     } else {
-    results[char]++;
+      console.log("space skipped");
     }
-   } else {
-    console.log("space skipped");
-   }
 
   }
   return results;
-}
- const counts = countLetters("lighthouse in the house")
-console.log("final counts", counts);
- 
+};
+
+module.exports = countLetters;
 
