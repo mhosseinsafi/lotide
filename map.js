490f5eb9a8353def7e0af531cd859a4e420a0a65
Hossein <hossein.saafi@gmail.com>
2023-05-12 23:03:33 -0700

    Map

--- map.js
+++ map.js
@@ -1,23 +1,6 @@
-const assertArraysEqual = function(arr1, arr2) {
 
-  if (eqArrays(arr1,arr2)) {
-    console.log(`🍏🍏Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🍎🍎Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-let eqArrays = function(arr1,arr2) {
-  let flag = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      flag = false;
-    }
-    if (arr1.length !== arr2.length) {
-      flag = false;
-    }
-  } return flag;
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const map = function(array, callback) {
@@ -28,12 +11,8 @@ const map = function(array, callback) {
   return results;
 };
 
-const words = ["hassan", "ali", "taghi", "naghi", "jamshid"];
+module.exports = map;
 
-
-
-const results1 = map(words, word => word[0]);
-assertArraysEqual(results1,words.map(x => x[0]));
 
 
 
