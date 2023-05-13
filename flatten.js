38c7f7345fc7019948c87de5000a9a56657e8fe1
Hossein <hossein.saafi@gmail.com>
2023-05-12 23:04:24 -0700

    Flatten

--- flatten.js
+++ flatten.js
@@ -1,23 +1,6 @@
-const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
-    console.log(`🍏🍏Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🍎🍎Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1,arr2) {
-  let flag = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      flag = false;
-    }
-    if (arr1.length !== arr2.length) {
-      flag = false;
-    }
-  } return flag;
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 const flatten = function(arr) {
   const output = [];
@@ -35,4 +18,5 @@ const flatten = function(arr) {
   return output;
 };
 
-assertEqual(eqArrays(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]),true); // => [1, 2, 3, 4, 5, 6]
+module.exports = flatten;
+
