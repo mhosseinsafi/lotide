6aa61492505ba5d8040d99ee1ac251052e648450
Hossein <hossein.saafi@gmail.com>
2023-05-12 23:03:06 -0700

    Take until

--- middle.js
+++ middle.js
@@ -1,26 +1,5 @@
 
 
-// let eqArrays = function(arr1,arr2) {
-//   let flag = true;
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       flag = false;
-//     }
-//     if (arr1.length !== arr2.length) {
-//       flag = false;
-//     }
-//   } return flag;
-// };
-
-// const assertArraysEqual = function(arr1, arr2) {
-
-//   if (eqArrays(arr1,arr2)) {
-//     console.log(`🍏🍏Assertion Passed: ${arr1} === ${arr2}`);
-//   } else {
-//     console.log(`🍎🍎Assertion Failed: ${arr1} !== ${arr2}`);
-//   }
-// };
-
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
--- takeUntil.js
+++ takeUntil.js
@@ -1,11 +1,5 @@
-const assertArraysEqual = function(arr1, arr2) {
 
-  if (eqArrays(arr1,arr2)) {
-    console.log(`🍏🍏Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🍎🍎Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const takeUntil = function(array, callback) {
   const result = [];
@@ -18,12 +12,4 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+module.exports = takeUntil;
