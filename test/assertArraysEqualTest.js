1910438d9252156ee5cf56d7c45d2d3582b419b6
Hossein <hossein.saafi@gmail.com>
2023-05-12 23:08:41 -0700

    Assert Array Equal Test

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,5 @@
 
-
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
