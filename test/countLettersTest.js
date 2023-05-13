737122acc7b3a21a354d2fab915f316aea333272
Hossein <hossein.saafi@gmail.com>
2023-05-12 23:09:52 -0700

    Count Letters Test

---
+++ test/countLettersTest.js
@@ -0,0 +1,6 @@
+
+const assert = require('chai').assert;
+const countLetters   = require('../countLetters');
+
+const counts = countLetters("lighthouse in the house")
+console.log("final counts", counts);
