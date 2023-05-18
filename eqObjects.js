c2c7842fbb87cb9657c9320aa269453c471fe1ee
Hossein <hossein.saafi@gmail.com>
2023-05-18 12:04:38 -0700

    Implement eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -3,25 +3,23 @@ const eqArrays = require('./eqArrays');
 const assertEqual = require('./assertEqual');
 
 const eqObjects = function(obj1,obj2) {
-  let flag = true;
-  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    flag = false;
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) { //to check the length of the object.
+    return false;
   }
-  for (const x in obj1) {
-    if (!obj2[x]) {
-      flag = false;
+  for (const x in obj1) { // x is indices of the obj1.
+    if (!obj2[x]) {  // if index x is not an index of obj2 , obj2[x] will be false.
+      return false;
     }
-    if (!Array.isArray(obj1[x])) {
+    if (!Array.isArray(obj1[x])) { // to check if the value of index x of obj1 is an array.
       if (obj1[x] !== obj2[x]) {
-        flag = false;
+        return false;
       } else {
         if (!eqArrays(obj1[x],obj2[x])) {
-          flag = false;
+          return false;
         }
       }
-    
-    } return flag;
+   
+    } return true; // if none of the above returns executed it means two objects are equal.
   }
 };
-
 module.exports = eqObjects;
