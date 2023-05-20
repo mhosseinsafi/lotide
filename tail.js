2d298953d4ebcc9bc552724d0a9c69a74c01ce66
Hossein <hossein.saafi@gmail.com>
2023-05-19 20:16:40 -0700

    remove unnecessary imports

--- tail.js
+++ tail.js
@@ -1,8 +1,6 @@
 
 // The tail is not the last element of the array. The tail is meant to be every element except the head (first element) of the array.
 
-const assertEqual = require('./assertEqual');
-
 let tail = function(arr) {
   let x = []; // x is the new variable which saves the new output 
   for (let i = 1; i < arr.length; i++) { // i start from 1 because i purposely want to skip the first element
